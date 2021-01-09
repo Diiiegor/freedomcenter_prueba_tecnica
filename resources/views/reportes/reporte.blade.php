d33c045a1dfa6ca4e95be440d35ba0bdfcfd8774
Juan Diego <joliveros@ieo.com.co>
2021-01-09 01:31:06 -0500

    Cambio la fuente del reporte

--- resources/views/reportes/reporte.blade.php
+++ resources/views/reportes/reporte.blade.php
@@ -6,11 +6,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <style>
         .container {
             width: 100%;
             text-align: center;
+            font-size: large;
         }
 
         .section--header {
@@ -58,13 +59,16 @@
         .red {
             background: rgb(255, 64, 70);
         }
+        *{
+            font-family: 'Poppins', sans-serif;
+        }
     </style>
 
 </head>
 <body>
 <header>
     <div class="container">
-        <h3>REPORTE DE ANIMALES POR CORRAL</h3>
+        REPORTE DE ANIMALES POR CORRAL
     </div>
 </header>
 
@@ -73,7 +77,7 @@
     @if(count($corral->animales)>0)
         <section>
             <div class="section--header">
-                <h3>CORRAL: {{$corral->nombre}}</h3>
+                CORRAL: {{$corral->nombre}}
             </div>
             <div class="section--body">
                 <table class="table">
